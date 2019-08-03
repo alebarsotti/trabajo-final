edf30b8cd53ac6218de9a4547aaaeaf5f97bcbbc
Alejandro Barsotti <alejandro_barsotti@hotmail.com>
2019-08-03 16:24:52 -0300

    Mejoras en formulario de detalles de medición Se agrega un botón para copiar el texto de los campos al portapapeles, y otro para limpiar todos los campos.

--- app/src/main/java/barsotti/alejandro/tf/activities/MeasurementDetailsActivity.java
+++ app/src/main/java/barsotti/alejandro/tf/activities/MeasurementDetailsActivity.java
@@ -1,20 +1,25 @@
 package barsotti.alejandro.tf.activities;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import barsotti.alejandro.tf.R;
@@ -24,10 +29,12 @@ public class MeasurementDetailsActivity extends AppCompatActivity {
     public static final String NEWLINE_CHAR = "\n";
     public static final String SEPARATOR = ": ";
     public static final String EMPTY_FORM_FIELD_TEXT = "-";
+    public static final String EMPTY_STRING = "";
     private static final String TAG = MeasurementDetailsActivity.class.getName();
     private static final String SCREENSHOTS_TAKEN_EXTRA = "screenshots_taken";
     private static final String ANGLE_MEASURES_EXTRA = "angle_measures";
     private static final String TOOTH_MEASURES_EXTRA = "tooth_measures";
+    public static final String MEASUREMENT_INFO_CLIPBOARD_LABEL = "measurement info";
     private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy - hh:mm",
         Locale.getDefault());
     private ArrayList<Uri> screenshotsTaken;
@@ -51,6 +58,53 @@ public class MeasurementDetailsActivity extends AppCompatActivity {
         rootViewGroup = findViewById(R.id.root);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.measurement_details, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+            case R.id.clear_fields:
+                clearFields();
+                return true;
+            case R.id.content_copy:
+                copyFieldsTextToClipboard();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void copyFieldsTextToClipboard() {
+        try {
+            String measurementInfoSummary = buildMeasurementInfoSummary();
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText(MEASUREMENT_INFO_CLIPBOARD_LABEL, measurementInfoSummary);
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, getString(R.string.copy_fields_toast_message), Toast.LENGTH_SHORT)
+                .show();
+        } catch (Exception e) {
+            Toast.makeText(this, getString(R.string.copy_fields_toast_error_message),
+                Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void clearFields() {
+        for (int i = 0; i < rootViewGroup.getChildCount(); i++) {
+            View child = rootViewGroup.getChildAt(i);
+            if (child instanceof EditText) {
+                ((EditText) child).setText(EMPTY_STRING);
+            }
+        }
+    }
+
     public static Intent getIntent(Context context, ArrayList<Uri> screenshotsTaken, String angleMeasures,
                                    String toothMeasures) {
         Intent intent = new Intent(context, MeasurementDetailsActivity.class);
@@ -68,7 +122,7 @@ public class MeasurementDetailsActivity extends AppCompatActivity {
     public void sendEmail(View view) {
         String subject = String.format(getString(R.string.default_mail_with_attachment_subject),
             dateFormat.format(new Date()));
-        String body = buildEmailBody();
+        String body = buildMeasurementInfoSummary();
         Intent mailIntent = MailUtils.composeEmailWithMultipleAttachments(subject, body, screenshotsTaken);
         if (mailIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mailIntent);
@@ -77,7 +131,7 @@ public class MeasurementDetailsActivity extends AppCompatActivity {
         }
     }
 
-    private String buildEmailBody() {
+    private String buildMeasurementInfoSummary() {
         StringBuilder bodyBuilder = new StringBuilder();
         for (int i = 0; i < rootViewGroup.getChildCount(); i++) {
             View child = rootViewGroup.getChildAt(i);
